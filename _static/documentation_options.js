35e3449c40f007e62a4ce9ba0f386b0c35f7a054
elariekerboull <larieker.e@gmail.com>
2026-03-03 10:32:15 +0000

    Deploying to gh-pages from @ elariekerboull/NuRadioMC@8a51346442772daeddeb37971796db971dfb9743 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.3.0-dev',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '3.2.0-dev',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
